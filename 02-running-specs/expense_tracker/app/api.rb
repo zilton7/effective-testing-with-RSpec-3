efc7eaded5d4003e3ad69791356c49aff683993f
Zil Norvilis <zilasino27@gmail.com>
2022-11-29 14:57:22 +0100

    Add get/post routes and server config

--- 02-running-specs/expense_tracker/app/api.rb
+++ 02-running-specs/expense_tracker/app/api.rb
@@ -3,5 +3,11 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+    post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
   end
 end
--- 02-running-specs/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ 02-running-specs/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -10,16 +10,44 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+    
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    
     it 'records submitted expenses' do
-      coffee = {
-        'coffee' => 'Starbucks',
+      pending 'Need to persist expenses'
+      # POST coffee, zoo, and groceries expenses here
+      zoo = post_expense(
+        'payee'  => 'Zoo',
+        'amount' => 15.25,
+        'date'   => '2017-06-10'
+      )
+
+      p zoo
+      
+      groceries = post_expense(
+        'payee'  => 'Whole Foods',
+        'amount' => 95.20,
+        'date'   => '2017-06-11'
+      )
+  
+      coffee = post_expense(
+        'payee' => 'Starbucks',
         'amount' => 5.75,
         'date' => '2017-06-10'
-      }
-
-      post '/expenses', JSON.generate(coffee)
-
+      )
+    
+      get '/expenses/2017-06-10'
       expect(last_response.status).to eq(200)
+      
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
   end
 end
